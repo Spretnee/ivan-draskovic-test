8645db7cc74fb3de75f29f6a7e003a0372ee216e
Ivan Draskovic <ivnddev@gmail.com>
2023-06-18 09:20:19 +0200

    Add bottom sheet plugin

--- App.tsx
+++ App.tsx
@@ -1,53 +1,18 @@
+import 'react-native-gesture-handler';
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 import {RootNavigator} from './src/navigation/RootNavigator';
-import TrackPlayer, {
-  AppKilledPlaybackBehavior,
-  Capability,
-} from 'react-native-track-player';
-import {useEffect} from 'react';
-import {PlayerStateProvider} from './src/providers/PlayerProvider';
+
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
 
 const queryClient = new QueryClient();
-const options = {};
-
-// const setupTrackPlayer = async () => {
-//   try {
-//     await TrackPlayer.setupPlayer();
-//     await TrackPlayer.updateOptions({
-//       android: {
-//         appKilledPlaybackBehavior: AppKilledPlaybackBehavior.ContinuePlayback,
-//       },
-//       capabilities: [
-//         Capability.Pause,
-//         Capability.Play,
-//         Capability.Stop,
-//         Capability.JumpBackward,
-//         Capability.JumpForward,
-//         Capability.Stop,
-//         Capability.SkipToNext,
-//         Capability.SkipToPrevious,
-//       ],
-//       compactCapabilities: [
-//         Capability.Play,
-//         Capability.Pause,
-//         Capability.Stop,
-//         Capability.JumpBackward,
-//         Capability.JumpForward,
-//         Capability.Stop,
-//         Capability.SkipToNext,
-//         Capability.SkipToPrevious,
-//       ],
-//     });
-//   } catch (e) {
-//     console.error('Track player setup error', e);
-//   }
-// };
 
 const App = () => {
   return (
-    <QueryClientProvider client={queryClient}>
-      <RootNavigator />
-    </QueryClientProvider>
+    <GestureHandlerRootView style={{flex: 1}}>
+      <QueryClientProvider client={queryClient}>
+        <RootNavigator />
+      </QueryClientProvider>
+    </GestureHandlerRootView>
   );
 };
 
